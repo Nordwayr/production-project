d9656dc70ee1d4da6eb2c026c7157f91ebfcfe4a
Nordwayr <nikolaev97@internet.ru>
2025-08-15 16:24:25 +0300

    add some config for webpack

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,18 @@
+import HTMLWebpackPlugin from "html-webpack-plugin";
+import { BuildOptions } from "./types/config";
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import webpack from "webpack";
+
+export function buildPlugins({paths} : BuildOptions): webpack.WebpackPluginInstance[] {
+
+    return [
+        new HTMLWebpackPlugin({
+            template: paths.html,
+        }),
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        })
+    ]
+}
